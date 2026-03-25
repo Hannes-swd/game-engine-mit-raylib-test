934d56d3d0a1b6a4cccdb4b054ca1b653d8dd050
hannes <hannesbauer347@gmail.com>
2026-03-25 14:52:47 +0100

    das edit fenster fonktioniert

--- game engine selber gemacht/edit.cpp
+++ game engine selber gemacht/edit.cpp
@@ -3,11 +3,30 @@
 #include "rlImGui.h"
 #include <cmath>
 #include <string>
+
 void zeichneEditFenster() {
-	ImGui::Begin("Edit Fenster");
-	ImGui::Separator();
-	std::string offenesObjeckt = "akktuelles objecktnahme";
-	ImGui::Text(offenesObjeckt);
-	ImGui::Separator();
-	ImGui::End();
+    ImGui::Begin("Edit Fenster");
+    ImGui::Separator();
+
+    if (selectedObjectIndex >= 0 && selectedObjectIndex < (int)objeckteListe.size()) {
+        Objeckte& aktuellesObjekt = objeckteListe[selectedObjectIndex];
+        ImGui::Text("Aktuelles Objekt: %s", aktuellesObjekt.name.c_str());
+
+        static char nameBuffer[256];
+        strcpy_s(nameBuffer, aktuellesObjekt.name.c_str());
+
+        if (ImGui::InputText("Name", nameBuffer, sizeof(nameBuffer))) {
+            aktuellesObjekt.name = nameBuffer;
+        }
+
+        ImGui::DragFloat("Position X", &aktuellesObjekt.PositionX);
+        ImGui::DragFloat("Position Y", &aktuellesObjekt.PositionY);
+        ImGui::ColorEdit4("Farbe", (float*)&aktuellesObjekt.farbe);
+    }
+    else {
+        ImGui::Text("Kein Objekt ausgewaehlt");
+    }
+
+    ImGui::Separator();
+    ImGui::End();
 }
